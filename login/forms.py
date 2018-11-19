99b599e8113f7d208687d4edd5aede0d43592536
pengboo <hz0126@163.com>
2018-11-19 17:28:38 +0800

    11.19

--- login/forms.py
+++ login/forms.py
@@ -1,2 +1,17 @@
 from django import forms
+from captcha.fields import CaptchaField
 
+class UserForm(forms.Form):
+    username = forms.CharField(label="用户名", max_length=128)
+    password = forms.CharField(label="密码", max_length=256, widget=forms.PasswordInput)
+    captcha = CaptchaField(label="验证码")
+
+
+class RegisterForm(forms.Form):
+    gender =(
+        ('male',"男"),
+        ('female',"女"),
+    )
+    username = forms.CharField(label="用户名", max_length=128, widget=forms.TextInput)
+    password1 = forms.CharField(label="密码", max_length=256, widget=forms.PasswordInput)
+    password2 = forms.CharField(label="确认密码", max_length=256, widget=forms.PasswordInput)
--- login/views.py
+++ login/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 from login import models
+from login import forms
 
 # Create your views here.
 
@@ -9,30 +10,42 @@ def index(request):
     return render(request, 'login/index.html')
 
 def login(request):
+    if request.session.get('is_login', None):
+        return redirect("/index/")
     if request.method == "POST":
-        username = request.POST.get('username', None)
-        password = request.POST.get('password', None)
-        message = "所有字段都必须填写！"
+        login_form = forms.UserForm(request.POST)
+        message = "请检查填写的内容！"
 
-        if username and password:
-            username = username.strip()
+        if login_form.is_valid():
+            username = login_form.cleaned_data['username']
+            password = login_form.cleaned_data['password']
 
             try:
                 user = models.User.objects.get(name=username)
                 if user.password == password:
+                    request.session['is_login'] = True
+                    request.session['user_id'] = user.id
+                    request.session['user_name'] = user.name
                     return redirect('/index/')
                 else:
                     message = "密码不正确！"
             except:
-                message = "用户名不存在！"
-        return render(request, 'login/login.html', {"message": message})
-    return render(request, 'login/login.html')
+                message = "用户不存在！"
+        return render(request, 'login/login.html', locals())
+
+    #对于非POST方法发送数据时，返回空表单，让用户可以填入数据
+    login_form = forms.UserForm()
+    return render(request, 'login/login.html', locals())
 
 def register(request):
     pass
     return render(request, 'login/register.html')
 
 def logout(request):
-    pass
+    if not request.session.get('is_login', None):
+        return redirect("/index/")
+    request.session.flush()
+
     return redirect("/index/")
+
 
