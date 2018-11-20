28e631d9cd613df7e223688f277cb85c5107701e
pe <hz0126@163.com>
2018-11-20 23:13:44 +0800

    2018.11.20.23:12

--- login/views.py
+++ login/views.py
@@ -40,6 +40,40 @@ def login(request):
 def register(request):
     if request.session.get('is_login', None):
         return redirect("/index/")
+    if request.method == "POST":
+        register_form = forms.RegisterForm(request.POST)
+        message = "请检查填写的内容！"
+        if register_form.is_valid():
+            username = register_form.cleaned_data['username']
+            password1 = register_form.cleaned_data['password1']
+            password2 = register_form.cleaned_data['password2']
+            email = register_form.cleaned_data['email']
+            sex = register_form.cleaned_data['sex']
+            if password1 != password2:
+                message = "两次输入的密码不同！"
+                return render(request, 'login/register.html', locals())
+            else:
+                same_name_user = models.User.objects.filter(name=username)
+                if same_name_user:
+                    message = "用户名已存在，请重新选择用户名！"
+                    return render(request, 'login/register.html', locals())
+                same_email_user = models.User.objects.filter(email=email)
+                if same_email_user:
+                    message = "该邮箱地址已被注册，请使用别的邮箱！"
+                    return render(request, 'login/register.html', locals())
+
+                new_user = models.User()
+                new_user.name = username
+                new_user.password = password1
+                new_user.email = email
+                new_user.sex = sex
+                new_user.save()
+                return redirect('/login/')
+
+    register_form = forms.RegisterForm()
+    return render(request, 'login/register.html', locals())
+
+
 
 def logout(request):
     if not request.session.get('is_login', None):
